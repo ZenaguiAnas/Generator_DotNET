1a20dda30573970f0a459c55152b5162df81c9c3
Anas Zenagui <zenaguianas20@gmail.com>
2024-06-20 19:42:42 +0100

    Initializing the project

---
+++ Generateur d_interface/DataBaseConnectionForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Generateur_d_interface
+{
+    partial class DataBaseConnectionForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.HostDB = new System.Windows.Forms.TextBox();
+            this.UsernameDB = new System.Windows.Forms.TextBox();
+            this.PasswordDB = new System.Windows.Forms.TextBox();
+            this.DatabaseName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ConnectDB = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HostDB
+            // 
+            this.HostDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.HostDB.Location = new System.Drawing.Point(287, 159);
+            this.HostDB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.HostDB.Multiline = true;
+            this.HostDB.Name = "HostDB";
+            this.HostDB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.HostDB.Size = new System.Drawing.Size(343, 36);
+            this.HostDB.TabIndex = 0;
+            this.HostDB.TextChanged += new System.EventHandler(this.HostDB_TextChanged);
+            // 
+            // UsernameDB
+            // 
+            this.UsernameDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.UsernameDB.Location = new System.Drawing.Point(287, 220);
+            this.UsernameDB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.UsernameDB.Multiline = true;
+            this.UsernameDB.Name = "UsernameDB";
+            this.UsernameDB.Size = new System.Drawing.Size(343, 36);
+            this.UsernameDB.TabIndex = 1;
+            // 
+            // PasswordDB
+            // 
+            this.PasswordDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.PasswordDB.Location = new System.Drawing.Point(287, 279);
+            this.PasswordDB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PasswordDB.Multiline = true;
+            this.PasswordDB.Name = "PasswordDB";
+            this.PasswordDB.Size = new System.Drawing.Size(343, 36);
+            this.PasswordDB.TabIndex = 2;
+            this.PasswordDB.UseSystemPasswordChar = true;
+            // 
+            // DatabaseName
+            // 
+            this.DatabaseName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.DatabaseName.Location = new System.Drawing.Point(287, 348);
+            this.DatabaseName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DatabaseName.Multiline = true;
+            this.DatabaseName.Name = "DatabaseName";
+            this.DatabaseName.Size = new System.Drawing.Size(343, 36);
+            this.DatabaseName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F);
+            this.label1.Location = new System.Drawing.Point(65, 159);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Host:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F);
+            this.label2.Location = new System.Drawing.Point(65, 220);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F);
+            this.label3.Location = new System.Drawing.Point(65, 279);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F);
+            this.label4.Location = new System.Drawing.Point(65, 348);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 39);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Database:";
+            // 
+            // ConnectDB
+            // 
+            this.ConnectDB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ConnectDB.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.ConnectDB.Location = new System.Drawing.Point(353, 436);
+            this.ConnectDB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ConnectDB.Name = "ConnectDB";
+            this.ConnectDB.Size = new System.Drawing.Size(164, 55);
+            this.ConnectDB.TabIndex = 8;
+            this.ConnectDB.Text = "Connect";
+            this.ConnectDB.UseVisualStyleBackColor = true;
+            this.ConnectDB.Click += new System.EventHandler(this.ConnectDB_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F);
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(157, 61);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(418, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Connection to the Database";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // DataBaseConnectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 544);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ConnectDB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DatabaseName);
+            this.Controls.Add(this.PasswordDB);
+            this.Controls.Add(this.UsernameDB);
+            this.Controls.Add(this.HostDB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(717, 591);
+            this.MinimumSize = new System.Drawing.Size(717, 591);
+            this.Name = "DataBaseConnectionForm";
+            this.Text = "UIGen - Database Connection";
+            this.Load += new System.EventHandler(this.DataBaseConnectionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox HostDB;
+        private System.Windows.Forms.TextBox UsernameDB;
+        private System.Windows.Forms.TextBox PasswordDB;
+        private System.Windows.Forms.TextBox DatabaseName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ConnectDB;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Generateur d_interface/OpsTablesForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Generateur_d_interface
+{
+    partial class OpsTablesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(514, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 33);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(415, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Table";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 62);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1013, 468);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // OpsTablesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 544);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(1082, 591);
+            this.MinimumSize = new System.Drawing.Size(1082, 591);
+            this.Name = "OpsTablesForm";
+            this.Text = "UIGen";
+            this.Load += new System.EventHandler(this.OpsTablesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
